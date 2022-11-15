189826377eb53e2f45beb15cddc7249766fc148a
MBahaa24 <mohamed.bahaa4@msa.edu.eg>
2022-11-15 19:12:20 +0200

    done

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
+
 // Require Express to run server and routes
 const express = require("express");
 // Start up an instance of app
@@ -12,11 +11,19 @@ const cors = require("cors");
 //enable cors req
 app.use(cors());
 
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
 
 // Get
-const GETAll =(req,res)=>{
-    res.status(200).send(projectData);
-}
+const GETAll =(req,res)=> res.status(200).send(projectData);
 app.get('/all',GETAll);
 
 
@@ -28,19 +35,15 @@ const post = (req,res)=>{
 }
 app.post("/add",post);
 
-//Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
-// Initialize the main project folder
-app.use(express.static('website'));
+
+
 
 // Setup Server
 const port = 4000;
 const hostname = "127.0.0.1";
 
-const listening = ()=>{
-    console.log(`server is running :${hostname} and ${port}`);
-}
+const listening = ()=>console.log(`server is running at http://${hostname}:${port}`);
+
 
 app.listen(port,listening);
--- website/app.js
+++ website/app.js
@@ -1,53 +1,57 @@
-/* Global Variables */
-
-const { json } = require("body-parser");
-const { Server } = require("http");
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.toDateString();
 
-// API Key
+
+// API Key & URL
 const URL = "https://api.openweathermap.org/data/2.5/weather?zip=";
-const apiKey = "095267cdbc63aff6740659d372b1e445";
+const apiKey = ",&appid=095267cdbc63aff6740659d372b1e445&units=metric";
 
+const server = "http://127.0.0.1:4000";
+
+const error = document.getElementById("error");
 
 const filldata = ()=>{
-    const zipid = document.getElementById("zip").ariaValueNow;
-    const feel = document.getElementById("feelings").ariaValueNow;
-}
+    const zip = document.getElementById("zip").value;
+    const feel = document.getElementById("feelings").value;
+    GetweatherData(zip).then((data)=>{
+        if(data)
+        {
+            const {
+                main: {temp},
+                name : city,
+                weather: [{description}],
+            } = data;
+    
+            const info = {
+                newDate,
+                city,
+                temp: Math.round(temp),
+                description,
+                feel,
+            };
+            postData(server+"/add",info)
+            updatingUI();
+            document.getElementById('entry').style.opacity = 1;
+        }
+    });
 
+};
 // Get the weather data
 
 document.getElementById("generate").addEventListener("click",filldata);
 
-GetweatherData(zipid).then((data)=>{
-    if(data)
-    {
-        const {
-            main: {temp},
-            name : city,
-            weather: [{description}],
-        } = data;
 
-        const info = {
-            newDate,
-            city,
-            temp: math.round(temp),
-            description,
-            feelings,
-        };
-        postData(Server+"/add",info)
-        updatingUI();
-        document.getElementById('entry').style.opacity = 1;
-    }
-});
 
-const GetweatherData=async(zipid)=>{
+const GetweatherData=async(zip)=>{
     try{
-        const res = await fetch(URL+zipid+apiKey);
+        const res = await fetch(URL+zip+apiKey);
         const data = await res.json();
-
+        if(data.cod != 200)
+        {
+            throw `${data.message}`;
+        }
         return data;    
     }
     catch(error){
@@ -59,7 +63,7 @@ const GetweatherData=async(zipid)=>{
 // function for posting data
 
 const postData = async(url="",info={})=>{
-const res = await fetch(url ,{
+    const res = await fetch(url ,{
     method:"post",
     headers:{
         "Content-Type":"application/json",
@@ -68,7 +72,7 @@ const res = await fetch(url ,{
 });
 try {
     const newData = await res.json();
-    console.log('You just saved',newData)
+    console.log(`You just saved`,newData)
     return newData;
 }catch (error){
     console.log(error);
@@ -76,16 +80,18 @@ try {
 };
 
 
-// showing the data 
+// updating The ui by the data
 
 const updatingUI = async ()=>{
-    const res = await fetch(Server+"/all");
+    const res = await fetch(server+"/all");
     try{
         const savedata = await res.json();
 
-        document.getElementById("date").innerHTML = savedata.newData;
+        document.getElementById("date").innerHTML = savedata.newDate;
         document.getElementById("temp").innerHTML = savedata.temp +'&degC';
-        document.getElementById("content").innerHTML =savedata.feelings;  
+        document.getElementById("content").innerHTML =savedata.description;
+        document.getElementById("city").innerHTML = savedata.city;
+        document.getElementById("feeling").innerHTML = savedata.feel; 
     }
     catch (error){
         console.log(error);
